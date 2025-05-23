abfc529cf7803c29297f3e282d949e3da2547331
vmora <vmora@ucenfotec.ac.cr>
2025-05-22 21:23:07 -0600

    [update] Archivo JS

--- JSON/js/index.js
+++ JSON/js/index.js
@@ -1,3 +1,15 @@
-/*
-Pendiente
-*/
+let personas;
+let divPersonas = document.getElementById("divPersona");
+
+//Función para cargar el JSON
+async function cargarJSON(){
+    const respuesta = await fetch("js/personas.json")
+    personas = await respuesta.json();
+
+    for(let i = 0; i < personas.length; i++){
+        divPersonas.innerText += personas[i].nombre + "\n";
+    } 
+}
+
+// Llamada a la función
+cargarJSON();
