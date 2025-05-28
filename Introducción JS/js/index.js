1597b2efeb5ed8355b4210be68acdb9ba9fc61f5
vmora <vmora@ucenfotec.ac.cr>
2025-05-27 22:04:51 -0600

    [update] index.html e index.js

--- Introducción JS/js/index.js
+++ Introducción JS/js/index.js
@@ -1,12 +1,29 @@
 // Variable: Espacio de memoria en la computadora, en donde un programa almacena un dato que puede o no cambiar durante la ejecución.  
 
 // Forma incorrecta de crear variables.
+/*var nombrePerro = "Kaiser";
 
+var nombreGato;
+nombreGato = "Rufus"; */
 
 // Forma adecuada de crear variables.
+let nombrePerro = "Kaiser";
+console.log("Variable: " + nombrePerro);
+
+nombrePerro = "Nevado";
+console.log("Variable: " + nombrePerro);
+
+
+let nombreGato;
+nombreGato = "Rufus";
+console.log("Variable: " + nombreGato);
 
 
 //Constante: Valor que no cambia durante la ejecución de un programa. 
+const PI = 3.14;
+console.log("Constante pi: " + PI);
+
+const NUMERO_MAXIMO_USUARIOS = 100;
 
 
 // Función: Bloque de código reutilizable que realiza una tarea específica.
@@ -19,15 +36,47 @@ Datos de prueba en kg       m       IMC
                     80      1.7     27.7
                     60      1.8     18.5
 */
+function calcularIMC(peso, estatura){
+    let imc = peso / Math.pow(estatura, 2)
+    console.log("El IMC es: " + imc.toFixed(2));
+}
+
+calcularIMC(80, 1.7);
+calcularIMC(60, 1.8);
+
+function calcularIMCRetorno(peso, estatura){
+    let imc = peso / Math.pow(estatura, 2)
+    return imc.toFixed(2);
+}
+
+console.log("El IMC es: " + calcularIMCRetorno(80, 1.7));
+console.log("El IMC es: " + calcularIMCRetorno(60, 1.8));
 
 
 /* Crear una función para calcular el área de un rectángulo
 Datos de prueba:    base    altura      área
                     5       3           15
                     4                   16 
+                            4           16
                     ""      2           Error
                     0       2           Error
 */
+function areaRectangulo(base, altura){
+    //Asignar altura = base si solo existe un dato de entrada
+    if (altura == undefined){ 
+        altura = base;
+    }
+    //Verificar que los datos de entrada son números
+    if (typeof base !== "number" || typeof altura !== "number" || base <= 0 || altura <= 0){
+        return "Error"
+    }
+    return base * altura;
+}
+
+console.log(areaRectangulo(5,3));
+console.log(areaRectangulo(4));
+console.log(areaRectangulo("4", 2));
+console.log(areaRectangulo(2, 0));
 
 
 /*
@@ -42,9 +91,32 @@ Datos de prueba:    Temperatura     Escala      Resultado esperado
                     0               a           Error
 */
 
+function convertirTemperatura(temperatura, escala){
+    //Validar los datos
+    if (typeof temperatura !== "number") {
+        return "Error: temperatura debe ser un número";
+    }
+    if (escala !== "C" && escala !== "F") {
+        return "Error: unidad no válida (use 'C' o 'F')";
+    }
+    // Conversión
+    let resultado = 0;
+    if (escala === "C"){
+        resultado = (temperatura - 32) * 5/9; // °F a °C
+    } else{
+        resultado = (temperatura *9/5) + 32; 
+    }
+    return resultado;
+}
+
+console.log(convertirTemperatura(25, "F"));
+console.log(convertirTemperatura(32, "C"));
+console.log(convertirTemperatura(32, "A"));
+console.log(convertirTemperatura("32", "C"));
+
 
 /*
-Ejercicio 2: Crear una función para verificar si una palabra es un palíndromo
+Ejercicio 2: Crear una función para verificar si una palabra es un palíndromo.
 
 Diseñar una función llamada esPalindromo que determine si una palabra se lee igual de izquierda a derecha que de derecha a izquierda.
 
